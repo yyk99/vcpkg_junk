954d693d94dc24f5b12dba54cf6add41d820a672
Yury Kuznetsov <yyk99@kestrel.home>
2025-04-04 21:15:37 -0400

    WIP

--- 08_assimp_junk/test_assimp.cpp
+++ 08_assimp_junk/test_assimp.cpp
@@ -10,6 +10,7 @@
 
 #include <filesystem>
 #include <string>
+#include <fstream>
 
 namespace fs = std::filesystem;
 
@@ -29,12 +30,32 @@ protected:
         fs::create_directories(ws);
         return ws;
     }
+
+    void save_as(std::string const &text, fs::path const &location)
+    {
+        std::ofstream os(location.string().c_str());
+        if(!os.good())
+            throw std::runtime_error("Cannot create file: " + location.string());
+        os << text;
+        os.close();
+    }
 };
 
 TEST_F(AssimpF, toolkit_create_ws) {
     auto ws = create_ws();
+
     ASSERT_TRUE(fs::is_directory(ws));
-    std::cout << ws << "\n";
+    save_as("hello\n", ws / "foo.txt");
+    ASSERT_TRUE(fs::is_regular_file(ws / "foo.txt"));
+
+    auto ws2 = create_ws();
+
+    ASSERT_EQ(ws, ws2);
+
+    ASSERT_TRUE(fs::is_directory(ws2));
+    ASSERT_FALSE(fs::is_regular_file(ws2 / "foo.txt"));
+    save_as("hello2\n", ws2 / "foo2.txt");
+    ASSERT_TRUE(fs::is_regular_file(ws2 / "foo2.txt"));
 }
 
 TEST_F(AssimpF, z1) {
