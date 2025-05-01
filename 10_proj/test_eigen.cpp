d2c0995207af0c90c17729750593c4a84f9d634f
Yury Kuznetsov <yyk99@kestrel.home>
2025-05-01 18:56:01 -0400

    Update test

--- 10_proj/test_eigen.cpp
+++ 10_proj/test_eigen.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 
 #include <Eigen/Sparse>
+#include <Eigen/Geometry>
 #include <proj.h>
 
 #include "../common/DebuggingConsole.h"
@@ -15,11 +16,11 @@ TEST_F(EigenF, version) {
     CONSOLE(EIGEN_WORLD_VERSION);
     CONSOLE(EIGEN_MAJOR_VERSION);
     CONSOLE(EIGEN_MINOR_VERSION);
+    EXPECT_EQ(3, EIGEN_WORLD_VERSION);
+    EXPECT_EQ(4, EIGEN_MAJOR_VERSION);
 }
 
 TEST_F(EigenF, t0) {
-    CONSOLE("Hello...");
-
     // clang-format off
     Eigen::Matrix4d yup2zup;
     yup2zup << 
@@ -37,4 +38,12 @@ TEST_F(EigenF, t0) {
 }
 
 TEST_F(EigenF, translate_transform) {
+    Eigen::Vector3f vv{1.0f, 2.0f, 3.0f};
+    Eigen::Translation<float, 3> T{1.0f, 2.0f, 3.0f};
+
+    CONSOLE_EVAL(vv.transpose());
+    vv = T * vv;
+    CONSOLE_EVAL(vv.transpose());
+
+    CONSOLE_EVAL(Eigen::Vector4f::Identity());
 }
