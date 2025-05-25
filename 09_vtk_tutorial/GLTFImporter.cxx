7710c84c09d212a9ab4f00d7ef10f75b726e74be
Yury Kuznetsov <yyk99@kestrel.home>
2025-05-25 05:31:32 -0400

    Add Axis.cxx

--- 09_vtk_tutorial/GLTFImporter.cxx
+++ 09_vtk_tutorial/GLTFImporter.cxx
@@ -29,7 +29,7 @@ int main(int argc, char* argv[])
   renderer->UseHiddenLineRemovalOn();
 
   vtkNew<vtkRenderWindow> renderWindow;
-  renderWindow->SetSize(640, 512);
+  renderWindow->SetSize(800, 600);
   renderWindow->AddRenderer(renderer);
   renderWindow->SetWindowName("GLTFImporter");
 
