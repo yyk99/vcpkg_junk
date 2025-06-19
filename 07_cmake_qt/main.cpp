0f22fe34df0cba409b1d5b1d745a6bfa4824e034
Yury Kuznetsov <yyk99@kestrel.home>
2025-06-19 09:14:38 -0400

    Update main.cpp
    Add "aboutQt" action

--- 07_cmake_qt/main.cpp
+++ 07_cmake_qt/main.cpp
@@ -2,8 +2,8 @@
 //
 //
 #include <QApplication>
-#include <QPushButton>
 #include <QMessageBox>
+#include <QPushButton>
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
@@ -11,9 +11,8 @@ int main(int argc, char *argv[]) {
     QPushButton button("Hello, Qt6!");
 
     QAction aboutAction("About", &button);
-    QObject::connect(&aboutAction, &QAction::triggered, [&]() {
-        QMessageBox::about(&button, "About", "This is a Qt6 application.");
-    });
+    QObject::connect(&aboutAction, &QAction::triggered,
+                     [&]() { QMessageBox::aboutQt(&button, "About"); });
     button.addAction(&aboutAction);
     button.setContextMenuPolicy(Qt::ActionsContextMenu);
     button.resize(200, 60);
